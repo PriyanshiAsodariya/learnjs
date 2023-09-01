ac28d84661544d6f9a54820c16b17ff82b88eff9
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-01 21:43:00 +0530

    variable

---
+++ js/variable.js
@@ -0,0 +1,22 @@
+    // var  firstname = "priyanshi";
+    // var firstname = "sanjana";
+
+    // document.write("my first name is ");
+
+    // document.write(firstname);
+
+
+    // var sum = 2 + 2;
+
+    // document.write(sum);
+
+
+    var firstname, lastname,age;
+
+    firstname = "priyanshi";
+    lastname = "asodariya";
+    age = 20;
+
+    document.write(firstname);
+    document.write(lastname);
+    document.write(age);
