a49540f5103a8be747f589b62b267876efee4e64
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-02 10:32:00 +0530

    variable

--- js/variable.js
+++ js/variable.js
@@ -9,8 +9,42 @@
 // var x;
 // console.log(x);   //10;
 
-var x = 10;
-{
-    var x = 20;
-}
-console.log(x);  //20;
+// var x = 10;
+// {
+//     var x = 20;
+// }
+// console.log(x);  //20;
+
+// let x = 10;
+// let x = 20;
+// console.log(x);   //error
+
+// let x = 10;
+// {
+//     let x = 20;
+// }
+// console.log(x);   //10
+
+// let x;
+// x = 10;
+// x = x + 5;
+// console.log(x);   //15
+
+
+// const x = 10;
+// const x = 20;
+// console.log(x);    //error
+
+// const x = 10;
+// {
+//     const x = 20;
+// }
+// console.log(x);   //10
+
+// const x;
+// x = 10;
+// console.log(x);    //error
+
+const x = 10;
+x = x + 5;
+console.log(x);  //error
