b9330a7146e3ac357a9d2dcf5e16a65b2d60c965
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-02 10:56:16 +0530

    variable

---
+++ js/cal.js
@@ -0,0 +1,28 @@
+
+let x = parseInt(prompt("enter value in x:",0));
+let y = parseInt(prompt("enter value in y:",0));
+
+let op = (prompt("enter operator"));
+
+console.log(x,y,op);
+
+
+
+switch(op){
+    case '+':
+        x = x + y;
+        console.log(x);
+        break;
+    case '-':
+        x = x - y;
+        console.log(x);
+        break;
+    case '*':
+        x = x * y;
+        console.log(x);
+        break;
+    case '/':
+        x = x / y;
+        console.log(x);
+        break;
+}
