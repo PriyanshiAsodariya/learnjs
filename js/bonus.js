1f990a60cbb5dd884baaae4e0bc8875011ea8930
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-04 10:19:24 +0530

    change

--- js/bonus.js
+++ js/bonus.js
@@ -2,20 +2,12 @@ function handlesubmit(){
     // console.log("ok");
 
     let salary = parseInt(document.getElementById("employee1").value);
-    let salary1 = parseInt(document.getElementById("employee2").value);
-
+    
     console.log("total salary:",salary);
     console.log("total salary:",salary1);
     let bonus;
 
-    // bonus = salary * 0.05;
-    // console.log("bonus is:",bonus);
-
-    // let bonus1;
-
-    // bonus1 = salary1 * 0.1;
-    // console.log("bonus is:",bonus1);
-
+    
     if(salary>5000){
         bonus = salary * 0.1;
         console.log(bonus);
