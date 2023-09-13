6db992dbc49a39e6cd61b460fbe45a3b2d5f93c5
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-13 12:07:24 +0530

    ok

--- js/keyboardevent.js
+++ js/keyboardevent.js
@@ -1,6 +1,6 @@
 function handleonkeydown(){
     console.log("okkkk");
-    let form = document.getElementById('form').style.backgroundColor=this.value; 
+    document.getElementById('form').style.backgroundColor=this.value; 
    
 }
 
