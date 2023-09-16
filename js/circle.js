5927290a863f0ba728a4082749289dd2791c4fc1
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-16 22:00:05 +0530

    ijkkl

--- js/circle.js
+++ js/circle.js
@@ -2,10 +2,20 @@ function handlesubmit(){
     // console.log("ok");
 
     let r = parseFloat(document.getElementById("radius").value);
-
+    if(r){
+        document.getElementById('error').innerHTML = ''
+    }else{
+        document.getElementById('error').innerHTML = 'enter radius'
+    }
+    
     let area;
 
     area = 3.14 * r * r;
     
     console.log(area);
+
+    document.getElementById('ans').innerHTML = area;
+    return false;
+
+    
 }
