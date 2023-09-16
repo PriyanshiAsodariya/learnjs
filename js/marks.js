e66904d369a75c2490e6561311b3de45651304b6
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-16 21:51:44 +0530

    OKK

--- js/marks.js
+++ js/marks.js
@@ -1,31 +1,107 @@
-function handlesubmit(){
+function handlesubmit() {
     // console.log("ok");
 
-    let m = parseFloat(document.getElementById("marks").value);
-    let m1 = parseFloat(document.getElementById("marks1").value);
-    let m2 = parseFloat(document.getElementById("marks2").value);
-    
-    let total;
+    let maths = parseFloat(document.getElementById("marks").value);
+    let science = parseFloat(document.getElementById("marks1").value);
+    let chemstry = parseFloat(document.getElementById("marks2").value);
 
-    total = m + m1 + m2 ;
+    let mathsE = true;
+    let sciE = true;
+    let chemE = true;
 
-    console.log("total marks is:",total);
 
-    let per;
 
-    per = total/3;
-
-    console.log("per is:",per);
-
-    if(per>80){
-        console.log("grade A");
-    }else if(per>70){
-        console.log("grade B");
-    }else if(per>50){
-        console.log("grade C");
-    }else{
-        console.log("grade D");
+    if (maths) {
+        if (maths >= 1) {
+            document.getElementById('mErr').innerHTML = ''
+            mathsE = false;
+        } else {
+            document.getElementById('mErr').innerHTML = 'please enter valid marks'
+        }
+    } else {
+        document.getElementById('mErr').innerHTML = 'enter marks'
     }
+
+    if (science) {
+        if (science >= 1) {
+            document.getElementById('m1Err').innerHTML = ''
+            sciE = false;
+        } else {
+            document.getElementById('m1Err').innerHTML = 'please enter valid marks'
+        }
+    } else {
+        document.getElementById('m1Err').innerHTML = 'enter marks'
+    }
+
+    if (chemstry) {
+        if (chemstry >= 1) {
+            document.getElementById('m2Err').innerHTML = ''
+            chemE = false;
+        } else {
+            document.getElementById('m2Err').innerHTML = 'please enter valid marks'
+        }
+    } else {
+        document.getElementById('m2Err').innerHTML = 'enter marks'
+    }
+
+    if (mathsE || sciE || chemE) {
+        return false
+    } else {
+        let total;
+        total = maths + science + chemstry;
+        console.log("total marks is:", total);
+
+        let per;
+        per = total / 3;
+        console.log("per is:", per);
+        let grade;
+        if (per > 80) {
+            console.log("grade A");
+            grade = 'A';
+        } else if (per > 70) {
+            console.log("grade B");
+            grade = 'B';
+        } else if (per > 50) {
+            console.log("grade C");
+            grade = 'C';
+        } else {
+            console.log("grade D");
+            grade = 'D';
+        }
+
+        document.getElementById('ans').innerHTML = grade;
+        return false;
+    }
+
+
+
+    // let total;
+
+    // total = maths + science + chemstry;
+
+    // console.log("total marks is:", total);
+
+    // let per;
+
+    // per = total / 3;
+
+    // console.log("per is:", per);
     
-     
+    // let grade;
+    // if (per > 80) {
+    //     console.log("grade A");
+    //     grade = A;
+    // } else if (per > 70) {
+    //     console.log("grade B");
+    //     grade = B;
+    // } else if (per > 50) {
+    //     console.log("grade C");
+    //     grade = C;
+    // } else {
+    //     console.log("grade D");
+    //     grade = D;
+    // }
+
+    // document.getElementById('ans').innerHTML = grade;
+    return false;
 }
