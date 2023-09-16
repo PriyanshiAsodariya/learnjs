17b577ddbd1da2469523ecab786dd492bdb56aa3
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-16 22:09:13 +0530

    okkk

--- js/bonus.js
+++ js/bonus.js
@@ -2,18 +2,37 @@ function handlesubmit(){
     // console.log("ok");
 
     let salary = parseInt(document.getElementById("employee1").value);
-    
+    let salaryE = true;
     console.log("total salary:",salary);
-    
-    let bonus;
 
-    
-    if(salary>5000){
-        bonus = salary * 0.1;
-        console.log(bonus);
-    }else if(salary<5000){
-        bonus = salary * 0.05;
-        console.log(bonus);
+    if(salary){
+        if(salary>=1){
+            document.getElementById('error').innerHTML = ''
+            salaryE = false;
+        }else{
+            document.getElementById('error').innerHTML = 'please enter valid salary'
+        }
+    }else{
+        document.getElementById('error').innerHTML = 'please enter salary'
     }
 
+    if(salaryE){
+        return false;
+    }else{
+        let bonus;
+        if(salary>5000){
+            bonus = salary * 0.1;
+            console.log(bonus);
+        }else if(salary<5000){
+            bonus = salary * 0.05;
+            console.log(bonus);
+        }
+
+        document.getElementById('ans').innerHTML = bonus;
+        return false;
+    }
+
+
+
+    return false;
 }
--- js/circle.js
+++ js/circle.js
@@ -1,13 +1,29 @@
 function handlesubmit(){
     // console.log("ok");
 
+
     let r = parseFloat(document.getElementById("radius").value);
+    let radiusE = true;
     if(r){
         document.getElementById('error').innerHTML = ''
+        radiusE = false;
     }else{
         document.getElementById('error').innerHTML = 'enter radius'
     }
-    
+
+    if(radiusE){
+        return false;
+    }else{
+        let area;
+
+        area = 3.14 * r * r;
+        
+        console.log(area);
+
+        document.getElementById('ans').innerHTML = area;
+        return false;
+    }
+
     let area;
 
     area = 3.14 * r * r;
