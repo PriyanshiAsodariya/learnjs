c1a3e351ca072dfe816b654db2ce56985d576934
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-16 22:33:52 +0530

    okk

--- js/scholarship.js
+++ js/scholarship.js
@@ -5,12 +5,42 @@ function handlesubmit(){
     console.log("your cast:",cast);
 
     let cgpa = parseFloat(document.getElementById("cgpa").value);
-
     console.log("cgpa is:",cgpa);
-
     let fees = parseInt(document.getElementById("fees").value);
-
     console.log("fees is :",fees);
+    let cgpaE = true;
+    let feesE = true;
+
+    // if(cast === '0'){
+    //     document.getElementById('castErr').innerHTML = 'select cast'
+    // }else{
+    //     document.getElementById('castErr').innerHTML = '';
+    // }
+
+    if(cgpa){
+        document.getElementById('cgpaErr').innerHTML = ''
+        cgpa = false
+    }else{
+        document.getElementById('cgpaErr').innerHTML = 'enter cgpa'
+    }
+
+    if(fees){
+        if(fees>=1){
+            document.getElementById('feesErr').innerHTML = ''
+            feesE = false
+        }else{
+            document.getElementById('feesErr').innerHTML = 'enter valud fees'
+        } 
+    }else{
+        document.getElementById('feesErr').innerHTML = 'enter fees'
+    }
+
+    if(cgpaE || feesE){
+        return false;
+    }else{
+
+    }
+
 
     let grade,scholarship,payablefees;
     if(cgpa>=9 && cgpa<=10){
@@ -19,6 +49,7 @@ function handlesubmit(){
         grade = 'B';
     }
     console.log( "grade is:",grade);
+    // document.getElementById('grade').innerHTML = 'grade';
 
     if(grade == 'A' || grade == 'B'){
         if(cast == 'o'){
@@ -30,14 +61,16 @@ function handlesubmit(){
             scholarship = fees * 0.5;
            
         }else if(cast == 'st'){
-            scholarshi = fees * 1;
+            scholarship = fees * 1;
             
         }
         console.log("scholarship is:",scholarship);
+        document.getElementById('ans').innerHTML = scholarship;
 
         payablefees = fees - scholarship;
         console.log("payablefees:",payablefees);
         
     }
 
+    return false;
 }
