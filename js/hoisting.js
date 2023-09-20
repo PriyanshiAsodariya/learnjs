b4dd57db9e0126e0e92ad8401fd85e7772670cd2
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-20 18:36:03 +0530

    okk

---
+++ js/hoisting.js
@@ -0,0 +1,38 @@
+
+x = 7             
+console.log(x);         // ans  7 variable can use first than after declared
+var x             
+
+
+
+console.log(y);      //undefined beacuse variable is not assgined
+var y     //declartion
+y=7       // assigment
+
+
+
+
+hello()              // ans good morining 
+
+function hello(){
+    console.log("good morning");      
+}
+
+
+hello1()                               // error genrated beacause curly bracket can not support hoisting
+    
+{function hello1(){
+    console.log("hello world");
+}}
+
+
+hello2();                             //error genreatde because function can not call before intialization
+                                    
+const hello2 = () => {
+    console.log("hello world");
+}
+
+
+
+
+
