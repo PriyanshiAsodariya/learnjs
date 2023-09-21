d09aea7514b6a3e415acd2b1186968b40df6c2c7
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-21 11:10:48 +0530

    okk

--- js/hoisting.js
+++ js/hoisting.js
@@ -1,50 +1,105 @@
             
-console.log(x);         // ans  7 variable can use first than after declared
-var x  
-x = 7            
+// console.log(x);         // ans  7 variable can use first than after declared
+// var x  
+// x = 7            
 
-x = 10;
-let x;                   //error beacuse let or const  can not decalrtion move to top
-console.log(x);
+// x = 10;
+// let x;                   //error beacuse let or const  can not decalrtion move to top
+// console.log(x);
             
 
 
-console.log(y);      //undefined beacuse variable is not assgined
-var y     //declartion
-y=7       // assigment1
+// console.log(y);      //undefined beacuse variable is not assgined
+// var y               //declartion
+// y=7                 // intiliaztion
 
 
 
-console.log(x);
-let x;
-x =20;
+// console.log(x);
+// let x;
+// x =20;
 
 
-console.log(x);
-x = 10;                //error
+// console.log(x);
+// x = 10;                //error
 
-x = 10;
-console.log(x);         //ans 10;
+// x = 10;
+// console.log(x);         //ans 10;
 
-hello()              // ans good morining 
+// hello()              // ans good morining 
 
-function hello(){
-    console.log("good morning");      
-}
+// function hello(){
+//     console.log("good morning");      
+// }
 
 
-hello1()                               // error genrated beacause curly bracket can not support hoisting
+// hello1()                    // error genrated beacause curly bracket can not support hoisting
     
-{function hello1(){
-    console.log("hello world");
-}}
+// {function hello1(){
+//     console.log("hello world");
+// }}
 
 
-hello2();                             //error genreatde because function can not call before intialization
+// hello2();                   //error genreated because arrow function can not call before intialization
                                     
-const hello2 = () => {
-    console.log("hello world");
+// const hello2 = () => {
+//     console.log("hello world");
+// }
+
+
+// function demo(){
+//     var x;
+//     x = 10;
+//     console.log(x);          //10  any variable let var const only declare in function scope
+// }
+
+// demo();
+
+// console.log(x);        //error beacuse not declare in outside of function
+
+
+// function demo(){     //function scope
+//     if(true){      //block scope
+//         var x;
+//         x = 10;
+//     }
+//     console.log(x);       //10
+// }
+
+// demo();
+
+
+// function demo(){
+//     let x;              // global scope + function scope
+//     if(true){         // block scope
+//         x = 10;
+//     }
+//     console.log(x);          //10;
+// }
+
+// demo();
+
+
+// function demo(){
+//     if(true){
+//         let x
+//         x = 10
+//     }
+//     console.log(x);             //error
+// }
+
+// demo();
+
+
+function demo(){
+    const x  = 10;
+    if(true){
+            
+        console.log(x);
+    }
+       
 }
+demo();
 
 
 
