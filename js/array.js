520ecb689a293c4e57373170fbbc7735f2a81993
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-22 09:33:35 +0530

    okk

---
+++ js/array.js
@@ -0,0 +1,86 @@
+let book1 = 'maths'
+let book2 = 'science'
+let book3 = 'computer'
+let book4 = 'english'
+
+console.log(book3);      // ans computer
+//----------------------------------------------
+
+
+// let book = new Array('maths' , 'science', 'computer', 'english');
+// console.log(book[3]);            //ans english
+
+//-----------------------------------------
+
+
+// let book = ['maths' , 'science', 'computer', 'english']
+// console.log(book[2]);                // ans computer
+
+//-------------------------------------
+
+// let book = ['maths' , 0.09 , 'priyanshi' , 'science', 'english']
+// console.log(book[1]);                   // ans 0.09 
+
+//------------------------------------------
+
+// let book = ['maths' , 'science', 'computer', 'english']
+// book[3] = 'bio';
+// console.log(book[3]);                    // ans bio            
+
+//--------------------------------------
+
+
+// let book = ['maths' , 'science', 'computer', 'english']
+
+// console.log(book);                   // ans full array
+
+//----------------------------------
+
+
+// let book = ['maths' , 'science', 'computer', 'english']
+
+// console.log(book.length);               // ans total length of array
+//------------------------------------------
+
+// let book = ['maths' , 'science', 'computer', 'english']
+
+// book.push('hindi')                              //   hindi add in last of array
+
+// console.log(book);                          // ans full array with hindi in add array
+//----------------------------------------------
+
+// let book = ['maths' , 'science', 'computer', 'english']
+
+// book.unshift('hindi')                              
+
+// console.log(book);                               // hindi add in before maths in full array
+
+//-----------------------------------------------------
+
+// let book = ['maths' , 'science', 'computer', 'english']
+
+// book.pop();
+
+// console.log(book);                                // remove last element in array
+//-----------------------------------------------
+
+// let book = ['maths' , 'science', 'computer', 'english']
+
+// book.shift()
+
+// console.log(book);                                  // remove first element in array
+//----------------------------------------------
+
+// let text = 'how are you'
+
+// let arrayword = text.split(' ')
+
+// console.log(arrayword);                                    // convert string into array
+
+//---------------------------------------------
+
+// let book = ['maths' , 'science', 'computer', 'english'];
+
+// let position = book.indexOf("scienece");
+
+// console.log(position);
