774041db782a0dfe0ceb084e4b07e1ee255f6210
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-09-26 11:27:05 +0530

    okk

--- js/array_task.js
+++ js/array_task.js
@@ -1,53 +1,63 @@
 
 let arr = [10, 7, 67, 44, 5]
-let max1 = arr[0]
+let arr2 = [10, 24, 10, 5, 67]
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max1) {
-        max1 = arr[i]
+const maxArr = (arr) => {
+    let max1 = arr[0]
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max1) {
+            max1 = arr[i]
+        }
     }
+    console.log(arr);
+    console.log("maximum number is : ", max1);
 }
-console.log(arr);
-console.log("maximum number is : ", max1);
+// maxArr(arr)
+
+//------------------------------------------------
 
 
 let number = [10, 7, 67, 44, 5]
 
-// let max = number.reduce((a, b) => a > b ? a : b)
-// console.log("maximum:", max);
-
-let max = number.reduce((acc,value)=>{
-    if(acc>value){
+let max = number.reduce((acc, value) => {
+    if (acc > value) {
         return acc;
-    }else{
+    } else {
         return value;
     }
 })
-console.log("maximum:",max);
+console.log("maximum:", max);
 
 
 //------------------------------------------
 
 
 
-let arr1 = [10, 7, 67, 44, 5]
-let min = arr1[0]
+// let arr1 = [10, 7, 67, 44, 5]
 
-for (let i = 0; i < arr1.length; i++) {
+const minArr = (arr) => {
+    let min = arr[0]
+
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] < min) {
             min = arr[i]
         }
     }
+    console.log(arr);
+    console.log("minimum number is: ", min);
+}
 
-console.log(arr1);
-console.log("minimum number is: ", min);
+// minArr(arr1)
 
-let arr2 = [10, 7, 67, 44, 5]
 
-let ans = arr2.reduce((acc,value)=>{
-    if(acc<value){
+
+// let arr = [10, 7, 67, 44, 5]
+
+let ans = arr.reduce((acc, value) => {
+    if (acc < value) {
         return acc
-    }else{
+    } else {
         return value
     }
 })
@@ -57,19 +67,24 @@ console.log("minimum:", ans);
 //---------------------------------------------
 
 
-let array = [10, 5, 22, 3, 45]
+// let array = [10, 5, 22, 3, 45]
 
-for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j<arr1.length; j++) {
-        if (array[i] > arr[j]) {
+const sortArr = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (array[i] > arr[j]) {
 
-            temp = array[i];
-            array[i] = array[j]
-            array[j] = temp;
+                temp = arr[i];
+                arr[i] = arr[j]
+                arr[j] = temp;
+            }
         }
     }
+    console.log("asecnding element", array);
+
 }
-console.log("asecnding element", array);
+// sortArr(arr)
+
 
 
 let array1 = [10, 5, 22, 3, 45]
@@ -79,21 +94,111 @@ console.log("asecnding: ", array1);
 
 //=======================================
 
-let num = [10, 5, 22, 3, 45]
+// let num = [10, 5, 22, 3, 45]
 
-for (let i = 0; i<num.length; i++) {
-    for (let j = i + 1; j<num.length; j++) {
-        if (num[i] < num[j]) {
+const decArr = (arr) => {
 
-            temp = num[i]
-            num[i] = num[j]
-            num[j] = temp
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
         }
     }
+    console.log("decending element:", num);
 }
-console.log("decending element:", num);
+
+// decArr(arr);
+
 
 let num1 = [10, 5, 22, 3, 45]
 
 num1.sort((a, b) => b - a)
 console.log("descending: ", num1);
+//===========================================
+
+// arr.reverse()
+// console.log(arr);
+
+//(reverse)
+
+//-------------------------
+
+const revArr = (arr2) => {
+    newArr = [];
+    for (let i = arr2.length - 1; i >= 0; i--) {
+        newArr.push(arr2[i])
+    }
+    console.log(newArr);
+}
+// revArr(arr2)
+
+//---------------------------
+
+//(sum)
+const sumArr = (arr2) => {
+    let sum = 0;
+    for (let i = 0; i < arr2.length; i++) {
+        sum = sum + arr2[i]
+    }
+    console.log("sum is:", sum);
+}
+// sumArr(arr2)
+
+//---------------------------------
+
+//(average)
+
+const aveArr = (arr2) => {
+    let sum = 0;
+    for (let i = 0; i < arr2.length; i++) {
+        sum = sum + arr2[i]
+    }
+    console.log("average is:", sum / arr2.length);
+}
+
+// aveArr(arr2);
+
+//----------------------------------
+
+//(check element)
+
+const checkelem = (arr2, elem) => {
+    let temp;
+    for (i = 0; i < arr2.length; i++) {
+        if (arr[i] === elem) {
+            temp = elem;
+            break;
+        }
+    }
+    if (temp) {
+        console.log(temp + "is available");
+    } else {
+        console.log(elem + "is not avialble");
+    }
+}
+
+// checkelem(arr2,99);
+
+//------------------------------------------
+
+const dupArr = (arr2) => {
+    for (i = 0; i < arr2.length; i++) {
+        for (j = i + 1; j < arr2.length; j++) {
+            if (arr2[i] === arr2[j]) {
+                for (k = j; k < arr2.length; k++) {
+                    arr2[k] = arr2[k + 1]
+                }
+                arr2.length--;
+            }
+        }
+    }
+    console.log(arr2);
+
+}
+// dupArr(arr2);
+
+//-------------------------------------------------
