3bdb00bf9d4b10819d6c7f1efd31ef81c631f7d7
PriyanshiAsodariya <asodariyapriyanshi@gmail.com>
2023-10-27 20:07:01 +0530

    ok

--- js/product_api.js
+++ js/product_api.js
@@ -11,16 +11,17 @@ const displayData = () =>{
                 console.log(data);
 
                 let print  = '';
+                print += `<div class ="containor">`
 
                 data.map((v)=>{
                     print +=   `<div id="box">`
                     print +=   `<div id="image_box"><img src="${v.image}" alt=""></div>`
-                    print +=   `<h2>${v.title}</h2>`
-                    print +=   `<h3>${v.price}</h3>`
-
-                    print += `</div>`
+                    print +=   `<div><h2>${v.title}</h2></div>`
+                    print +=   `<div><h3>${v.price}</h3></div>`
+                    print +=   `</div>`
 
                 })
+                print += `</div>`
 
                 document.getElementById("main_box").innerHTML = print;
                 
@@ -37,7 +38,36 @@ const displayData = () =>{
 
 const handleSearch = () =>{
     // get the value from search box
-    let searchBoxValue = document.querySelector("#search").value;
+   try{
+    fetch("https://fakestoreapi.com/products")
+        .then((res)=>{
+            if(res.ok){
+                return res.json()
+            }
+            throw new Error ("something went wrong")
+        })
+        .then((data)=>{
+            let searchvalue = document.getElementById("search").ariaValueMax.toLowerCase();
+            let print = '';
+            data.filter((v)=>{
+                if(v.title.toLowerCase().includes(searchvalue)){
+                print +=   `<div id="box">`
+                print +=   `<div id="image_box"><img src="${v.image}" alt=""></div>`
+                print +=   `<h2>${v.title}</h2>`
+                print +=   `<h3>${v.price}</h3>`
+
+                print += `</div>`
+                }
+
+            })
+            document.getElementById("result").innerHTML = print;
+        })
+
+   }catch(error){
+    console.log(error);
+
+   }
+
    
 
    
